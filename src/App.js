640369acb454c9fe728b97b9de70db0b2c1d05dd
kislo-Luis <luiskislo15@gmail.com>
2022-12-14 22:39:24 -0300

    feat: white screen

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
-//import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import  Example  from './components/navigation/NavBar';
+
+
 
 function App() {
+
   return (
-    <div className="App">
+    
+
+      
+      <div></div>
      
-     
-    </div>
+   
+
   );
 }
 
 export default App;
-
-//clean code for white page
-
-{/* <header className="App-header">
-<img src={logo} className="App-logo" alt="logo" />
-<p>
-  Edit <code>src/App.js</code> and save to reload.
-</p>
-<a
-  className="App-link"
-  href="https://reactjs.org"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  Luchito React
-</a>
-</header> */}

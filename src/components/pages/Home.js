3019778505c6774a897d155e7ea934357c6e14db
kislo-Luis <luiskislo15@gmail.com>
2023-01-05 18:07:47 -0300

    feat:card-component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,26 +1,39 @@
-import { Button } from "reactstrap";
+import {
+  Button,
+  Card,
+  CardText,
+  CardSubtitle,
+  CardTitle,
+  CardBody,
+} from "reactstrap";
 import { Link } from "react-router-dom";
 export const Home = () => {
   return (
     <div>
-      <h1>Isotopos de Springfield</h1>
-
-      <p>
-        Los Isotopos de Springfield son un equipo de béisbol profesional en el
-        que ha jugado Bart Simpson en algunos episodios. Eran los ídolos de
-        Homero Simpson hasta que se harto de su racha perdedora. Luego cuando
-        volvieron a ganar otra vez, Homer volvió a hacerse fan de el equipo. En
-        uno de los episodios, el dueño del equipo lo quiere cambiar a
-        Albuquerque. En este mismo episodio, Homer comienza una huelga de hambre
-        en protesta por el cambio, aunque la afición no le creía.
-      </p>
+      <Card
+        color="primary"
+        style={{
+          width: "18rem",
+        }}
+      >
+        <img alt="Sample" src="/isotopes.png" />
+        <CardBody>
+          <CardTitle tag="h5">Isotopos de Springfield</CardTitle>
+          <CardSubtitle className="mb-2 text-muted" tag="h6">
+            ...de albuquerque?
+          </CardSubtitle>
+          <CardText>
+            Los Isotopos de Springfield son un equipo de béisbol profesional en
+            el que ha jugado Bart Simpson en algunos episodios.
+          </CardText>
+          <Link to="#">
+            <Button color="dark"> Leer más... </Button>
+          </Link>
+        </CardBody>
+      </Card>
 
       <Link to="/menu">
         <Button color="primary"> Atras </Button>
-      </Link>
-
-      <Link to='#'>
-        <Button color="dark"> Leer más... </Button>
       </Link>
     </div>
   );

2dd6e5e1ae63218f6647169781c412fc3a343a1e
Kislo <luiskislo15@gmail.com>
2023-01-31 19:12:20 -0300

    fix:home-cards-props

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,18 @@
 import React from "react";
 import { Card } from "../components/card/Card";
-import {cardsData} from '../components/card/cardsData'
-
-
-
-
-
+import { cardsData } from "../components/card/cardsData";
 
 export const Home = (props) => {
   return (
     <div>
-      
-       { cardsData.map(data =>(
-          <Card
+      {cardsData.map((data) => (
+        <Card
           key={data.id}
           title={data.title}
           content={data.content}
           img={data.img}
-          />
-
-
-       ))}
-     
-          
-      
-      
-      
-       
+        />
+      ))}
     </div>
   );
 };
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,32 +1,12 @@
 import React from "react";
-import { Card } from "../components/card/Card";
-import {cardsData} from '../components/card/cardsData'
 
 
-
-
-
-
-export const Profile = (props) => {
+export const Profile = () => {
   return (
     <div>
-      
-       { cardsData.map(data =>(
-          <Card
-          key={data.id}
-          title={data.title}
-          content={data.content}
-          img={data.img}
-          />
 
-
-       ))}
      
-          
-      
-      
-      
-       
+
     </div>
   );
 };

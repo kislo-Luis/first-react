9cfe71e3d32af7c5df8c96f526c5d20325c0fde3
Kislo <luiskislo15@gmail.com>
2023-02-03 16:47:56 -0300

    feat:grid

--- src/components/card/cardsData.js
+++ src/components/card/cardsData.js
@@ -9,31 +9,31 @@ export const cardsData =  [
   {
     id: 2,
     title: "Isotopos de Albuquerque",
-    content: "Son un equipo de béisbol que intentan desaparecer a los Isotopos de Springfield.",
+    content: "Son un equipo de béisbol que intentan desaparecer a los Isotopos de Springfield, pero Homero se entera.",
     img:"https://picsum.photos/300/200",
   },
   {
     id: 3,
     title: "Isotopos de Guayubira",
-    content: "Son el equipo de buena madera, los mas duros.",
+    content: "Son el equipo de buena madera y de Misiones, los mas duros, algunos jugadores eran de rugby anteriormente.",
     img:"https://picsum.photos/300/200",
   },
   {
     id: 4,
     title: "Neutrones",
-    content: "Es el equipo más tibio de la tabla, siempre empatan o pierden, nunca ganan",
+    content: "Es el equipo más tibio de la tabla, siempre empatan o pierden, nunca ganan, suman para abajo perdieron socios.",
     img:"https://picsum.photos/300/200",
   },
   {
     id: 5,
     title: "Atomicos",
-    content: "El equipo más explosivo del campeonato, tienen 11 expulsados en lo que va del torneo. ",
+    content: "El equipo más explosivo del campeonato, tienen 11 expulsados en lo que va del torneo, algunas fechas no se presentaron. ",
     img:"https://picsum.photos/300/200",
   },
   {
     id: 6,
     title: "Don electron",
-    content: "El conjunto más estrategico del campeonato, siempre suma puntos.",
+    content: "El conjunto más estrategico del campeonato, siempre suma puntos, en algun momento cambiaron su nombre. ",
     img:"https://picsum.photos/300/200",
   },
 ]
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Card } from "../components/card/Card";
-import { cardsData } from "../components/card/cardsData";
-
+import { Card } from "../../components/card/Card";
+import { cardsData } from "../../components/card/cardsData";
+import './Home.css';
 export const Home = (props) => {
   return (
-    <div>
+    <div className="cards-grid">
       {cardsData.map((data) => (
         <Card
           key={data.id}
